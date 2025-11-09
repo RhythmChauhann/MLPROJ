69b151e59c47548e2541b0719e2b6036e3eb6b28
RhythmChauhann <skrilzeef@gmail.com>
2025-11-09 14:08:00 +0530

    final commit with working pipeline

--- src/mlproj/components/model_trainer.py
+++ src/mlproj/components/model_trainer.py
@@ -19,6 +19,16 @@ from src.mlproj.exception import CustomException
 from src.mlproj.logger import logging
 from src.mlproj.utils import save_object, evaluate_models
 
+import numpy as np
+import mlflow
+import mlflow.sklearn
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from urllib.parse import urlparse
+import dagshub
+dagshub.init(repo_owner='rhythmchauhann',
+             repo_name='MLPROJ',
+             mlflow=True)
+
 
 @dataclass
 class ModelTrainerConfig:
@@ -27,6 +37,12 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
+    
+    def eval_metrics(self,actual,pred):
+         rmse= np.sqrt(mean_squared_error(actual,pred))
+         mae = mean_absolute_error(actual,pred)
+         r2 = r2_score(actual,pred)
+         return rmse, mae,r2
 
     def initiate_model_trainer(self,train_array,test_array):
             try:
@@ -85,12 +101,57 @@ class ModelTrainer:
                 }
                 
                 model_report:dict = evaluate_models(X_train,y_train,X_test,y_test,models,params)
-
+                #to get the best model form the dictionary
                 best_model_score = max(sorted(model_report.values())) 
+                #to get the best model namee from dictionary
                 best_model_name=list(model_report.keys())[
                     list(model_report.values()).index(best_model_score)
                 ]
                 best_model = models[best_model_name]
+                print(f"This is the best model :{best_model_name}")
+                model_names = list(params.keys())
+                actual_model = ""
+                for model in model_names:
+                     if model == best_model_name:
+                          actual_model = actual_model+model
+                best_params = params[actual_model]
+
+                mlflow.set_registry_uri("https://dagshub.com/RhythmChauhann/MLPROJ.mlflow")
+                tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+                #mlflow pipeline
+                with mlflow.start_run():
+
+                    predicted_qualities = best_model.predict(X_test)
+
+                    (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+                    mlflow.log_params(best_params)
+
+                    mlflow.log_metric("rmse", rmse)
+                    mlflow.log_metric("r2", r2)
+                    mlflow.log_metric("mae", mae)
+
+
+                    # Model registry does not work with file store
+                    if tracking_url_type_store != "file":
+
+                        # Register the model
+                        # There are other ways to use the Model Registry, which depends on the use case,
+                        # please refer to the doc for more information:
+                        # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                        try:
+                            mlflow.sklearn.log_model(best_model, "model")
+                        except Exception as e:
+                            print("⚠️ Model logging to DagsHub skipped due to unsupported endpoint:", e)
+                            mlflow.sklearn.save_model(best_model, "artifacts/model")
+                            mlflow.log_artifact("artifacts/model")
+
+                    else:
+                        mlflow.sklearn.save_model(best_model, "artifacts/model")
+                        mlflow.log_artifact("artifacts/model")
+
+
+
                 if best_model_score<0.6:
                     raise CustomException("No Best Model Found")
                 logging.info("Best model found on both training and test data")
